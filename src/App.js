f1470e8d2c3b9015a8d38b74df5251701226a387
tannu92 <tannu92@outlook.com>
2021-07-15 17:34:06 +0530

    added all sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import React from 'react';
-
-// components
-
+import React from "react";
 import MyNavbar from "./components/my-navbar/mynavbar";
 import MyCarousal from "./components/my-carousal/mycarousal";
 import TitleMessage from "./components/title-message/title-message";
+import About from "./pages/about/about";
+import Skills from "./pages/skills/skills";
+import Experience from "./pages/experience/experience";
+import TimeLine from "./components/projects-timeline/projects-timeline";
+import ContactForm from "./pages/contact-form/contact-form";
+import Container from "react-bootstrap/Container";
+
+
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
+
+import { Parallax } from "react-parallax";
+import Particles from "react-particles-js";
+import { particlesOptions } from "./particlesOptions";
+import FooterPanel from "./components/footer/footer";
 
 
 import './App.css';
 
-const App=()=> {
+const App = () => {
   return (
-    <div>
-    <MyNavbar/>
-    <MyCarousal/>
-    <TitleMessage/>
+    <div className="App" style={{ position: "relative" }}>
+      <MyCarousal />
+      <TitleMessage />
+      <MyNavbar />
+      {<Particles
+        className="particles particles-box"
+        params={particlesOptions}
+      /> }
+
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+       <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <hr />
+
+            <Experience />
+          </Fade>
+        </Container>
+      </div>
+
+       <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <TimeLine />
+        </Slide>
+      </Container>
+       <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+          <ContactForm />
+        </Fade>
+      </Container>
+
+      <hr />
+
+    <FooterPanel/>
       
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/about/about.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.style.css";
+import Profile from "../../assets/img/profile/profile.jpg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+           
+           <Container>
+              <Row className="pt-3 pb-5 align-items-center">
+                 
+                 <Col xs={12} md={6}>
+                      <Row className="justify-content-center mb-2 mr-2 ">
+                      <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+                      </Row>
+                </Col>
+            
+                <Col xs={12} md={6}>
+                     <Row className=" align-items-start p-2 my-details rounded">
+                          Hi there! I am <strong>&nbsp;Tannu</strong>
+                          <br />A passionate programmer, born and brought up in India. I am a Web Developer with React.js, Node.js, Javascript, HTML and CSS as my tech stack.
+                          <br />
+                          In 2016, I successfully completed my Engineering with specialization in 'Computer Science'.
+                          <br />
+                          Working with the clients, my goal is always driven towards providing amazing experience with the best level of quality and service to them.
+                          
+                          <br />I love learning about new technologies, what problems are they solving and how can I use them to build better and scalable products.
+                          <br /> <br />
+              
+                          <Col className="d-flex justify-content-center flex-wrap">
+                               <div>
+                               <a href="#contact">
+                               <Button className="m-2" variant="outline-primary">
+                                Let's talk
+                               </Button>
+                               </a>
+                               </div>
+                  
+                              <div>
+                              <a href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                              <Button className="m-2" variant="outline-success">
+                               My Resume
+                              </Button>
+                              </a>
+                              </div>
+                     
+                              <div>
+                              <a href="https://github.com/tannu92" target="_blank" rel="noopener noreferrer">
+                              <Button className="m-2" variant="outline-dark">
+                              GitHub
+                              </Button>
+                              </a>
+                              </div>
+                    
+                              <div>
+                              <a href="https://www.linkedin.com/in/tannu-mendiratta-5b812aba/" target="_blank" rel="noopener noreferrer">
+                              <Button className="m-2" variant="outline-info">
+                               LinkedIn
+                              </Button>
+                              </a>
+                              </div>
+                          </Col>
+                      </Row>
+                </Col>
+            </Row>
+
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
